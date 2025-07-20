6848d766e32eec9824c018ed48068c4a8d282408
umme-aiman20 <136161506+umme-aiman20@users.noreply.github.com>
2025-07-20 19:54:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from stable_baselines3.common.torch_layers import NatureCNN
 from stable_baselines3.ppo import CnnPolicy
 
 env = RobotArmPickPlaceEnv(render=False)
-check_env(env, warn=True)  # This should now pass ✅
+check_env(env, warn=True)  
 
 model = PPO("CnnPolicy", env, verbose=1)
 model.learn(total_timesteps=10000)
